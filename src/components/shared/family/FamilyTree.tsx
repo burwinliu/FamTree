2cb1ed16e017284e95551abbbeb3c3ed8080c34a
burwinliu <burwincollege@gmail.com>
2024-06-22 01:23:01 -0700

    [Fix #4] Render out Tree in center
    TODO:
    * Finish #3
    * Finish writing issues for allowing for focus on specific people
    * Submit data for remaining values

--- src/components/shared/family/FamilyTree.tsx
+++ src/components/shared/family/FamilyTree.tsx
@@ -1,25 +1,54 @@
-import React, { MutableRefObject, useRef } from "react";
-import { Canvas } from 'reaflow';
+import React, { MutableRefObject, useRef, useState } from "react";
+import { Canvas, CanvasPosition, CanvasRef } from 'reaflow';
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch"
 
 import MemberNode from "components/features/tree/MemberNode"
 import { FamilyTreeProps } from 'types/tree/Tree';
 import { Tree } from 'models/Tree';
+import { debounce } from "lodash";
+import { useEffect } from "react";
 
 export default function FamilyTree(props: FamilyTreeProps) {
+    const canvasRef = useRef<CanvasRef>(null);
     const tree: MutableRefObject<Tree> = useRef(props.tree);
+    const [height, setHeight] = useState<number>(0);
+    const [width, setWidth] = useState<number>(0);
+    const familyWrapperRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (familyWrapperRef && familyWrapperRef.current) {
+            setWidth(familyWrapperRef.current!.clientWidth-5);
+            setHeight(familyWrapperRef.current!.clientHeight-5);
+        }
+    }, [familyWrapperRef.current?.clientWidth, familyWrapperRef.current?.clientHeight]);
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         canvasRef?.current?.fitCanvas?.();
+    //         console.log("FITTED");
+    //     }, 100);
+    // }, [canvasRef]);
 
     return (
-        <TransformWrapper wheel={{ step: 40 }} maxScale={4} limitToBounds={false}>
-            <TransformComponent>
-                <Canvas
-                    fit={true}
-                    readonly={true}
-                    nodes={tree.current.nodeData}
-                    edges={tree.current.edgeData}
-                    node={MemberNode}
-                />
-            </TransformComponent>
-        </TransformWrapper>
+        <div ref={familyWrapperRef} style={{ height: "100%", width: "100%", maxHeight: "100%", maxWidth: "100%" }}>
+            <TransformWrapper wheel={{ step: 40 }} minScale={0.5} maxScale={4} initialScale={1} limitToBounds={false}>
+                <TransformComponent>
+                    <Canvas
+                        ref={canvasRef}
+                        zoomable={false}
+                        fit={true}
+                        animated={false}
+                        readonly={true}
+                        nodes={tree.current.nodeData}
+                        edges={tree.current.edgeData}
+                        node={MemberNode}
+                        defaultPosition={CanvasPosition.CENTER}
+                        maxHeight={height}
+                        maxWidth={width}
+                    />
+                </TransformComponent>
+            </TransformWrapper>
+        </div>
+
     );
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,12 +3,41 @@ import './App.css';
 import FamilyTree from "components/shared/family/FamilyTree";
 import { Tree } from 'models/Tree';
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
 function App() {
   return (
-    <div className="App">
-      {/* TODO: @burwinliu - Add Issue to allow for selector between family lines */}
-      <FamilyTree tree={new Tree("croissant")}/>
-    </div>
+    <Box sx={{
+      height: '100vh',
+      maxHeight: '100vh',
+    }}>
+      <AppBar position="sticky">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Family Tree
+          </Typography>
+          <Button color="inherit">Switch Families</Button>
+        </Toolbar>
+      </AppBar>
+      <Grid
+        container
+        sx={{height: '92.5%', width: '100%'}}
+      >
+        <Grid
+          item
+          className='canvas-wrapper'
+          sm={true}
+        >
+          {/* TODO: @burwinliu - Add Issue to allow for selector between family lines */}
+          <FamilyTree tree={new Tree("croissant")} />
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
